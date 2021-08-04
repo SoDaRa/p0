b854b627be6714f24969f9585093e7dfb075f718
SoDaRa <46580818+SoDaRa@users.noreply.github.com>
2021-08-04 12:02:07 -0400

    Made Connect 4 Check Win Test more Robust
    Forgot to do it in last commit.

--- src/test/java/ConnectFourBoardTest.java
+++ src/test/java/ConnectFourBoardTest.java
@@ -22,6 +22,7 @@ class ConnectFourBoardTest {
     @Test
     void checkWin() {
         var my_board = new ConnectFourBoard(6,7,4);
+        // Player 1 Win
         my_board.addPiece(1,1);
         my_board.addPiece(2,-1);
         my_board.addPiece(2,1);
@@ -35,5 +36,21 @@ class ConnectFourBoardTest {
         my_board.addPiece(4,-1);
         my_board.addPiece(4,1);
         assertEquals(2,my_board.checkWin());
+        // Player 2 Win
+        my_board = new ConnectFourBoard(6,7,4);
+        for (int i = 0; i < 4; i++)
+            my_board.addPiece(i, -1);
+        assertEquals(-2, my_board.checkWin());
+        // Draw Check
+        my_board = new ConnectFourBoard(3,3,3);
+        my_board.addPiece(0,1);
+        my_board.addPiece(0,-1);
+        my_board.addPiece(1,1);
+        my_board.addPiece(2,-1);
+        my_board.addPiece(2,1);
+        my_board.addPiece(1,-1);
+        my_board.addPiece(0,1);
+        my_board.addPiece(1,-1);
+        assertEquals(-10, my_board.checkWin());
     }
 }
